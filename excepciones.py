5ab3b36485f9f5496dbc9b4c13b8147ad8ee4c01
Joge Gil <alexgilma@hotmail.com>
2024-10-16 10:32:13 -0500

    change: new exception

--- excepciones.py
+++ excepciones.py
@@ -6,7 +6,7 @@ valido = False
 while not valido:
     try:
         valido = True
-        numero = 33 #input('Dame un número romano: ')
+        numero = input('Dame un número romano: ')
         valor_convertido = romano_a_entero(numero)
         print(valor_convertido)
         valido = True
@@ -16,3 +16,10 @@ while not valido:
     except TypeError as ex2:
         print('Solamente admito cadenas')
         print(ex2)
+    except IndexError as ex3:
+        print('No puedo admitir Dobles VV, LL, DD')
+        print(ex3)
+    except IndexError as ex4:
+        print('No son un dígitos romanos válidos: (I, V, X, L, C, D, M)')
+        print(ex3)        
+    
